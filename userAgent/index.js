4466216c2fb4229dd0371f0f1d7d952c495a753a
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-05-17 12:12:35 +0530

    User Agent

---
+++ userAgent/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+// const useragent = require('express-useragent')
+const UAParser = require('ua-parser-js')
+
+const app = express();
+
+const PORT = 4000
+
+// app.use(useragent.express())
+// app.get('/', (req, res) => {
+//     console.log(req.useragent)
+// })
+
+app.get('/', (req, res) => {
+    const parser = new UAParser();
+    const userAgent = req.headers['user-agent'];
+    const uaResult = parser.setUA(userAgent).getResult();
+    console.log(uaResult)
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
